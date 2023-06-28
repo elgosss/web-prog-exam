6057c32733e62508bbb818f0ea7b68f7102fe83b
Gorokhova Elena <125600432+elgosss@users.noreply.github.com>
2023-06-29 01:26:17 +0300

    Add files via upload

--- sender.php
+++ sender.php
@@ -1,21 +1,18 @@
 <?php
-    if (isset($_GET['submit'])) {
-        $name = $_GET['fb_name'];
-        $surname = $_GET['fb_surname'];
-        $phone = $_GET['fb_phone'];
-        $email = $_GET['fb_email'];
-        $text = $_GET['fb_text'];
+    $name = trim($_POST['fb_name']);
+    $surname = trim($_POST['fb_surname']);
+	$phone = trim($_POST['fb_phone']);
+    $email = trim($_POST['fb_email']);
+    $text = trim($_POST['fb_text']);
 
-        $to = "e.s.gorohova@gmail.com"; 
-        $subject = "Обратная связь";
+	$to = "e.s.gorohova@gmail.com"; 
+	$subject = "Обратная связь";
 
-        $msg="
-        Имя: $name /n
-        Фамилия: $surname /n
-        Телефон: $phone /n
-        Адрес эл. почты: $email /n
-        Сообщение: $text"; 	
-        mail($to, $subject, $msg, "Content-type: text/plain; charset=\"utf-8\"\n From: $to");
-    }
-    
+	$msg="
+    Имя: $name /n
+    Фамилия: $surname /n
+    Телефон: $phone /n
+    Адрес эл. почты: $email /n
+    Сообщение: $text"; 	
+	mail($to, $subject, $msg, "Content-type: text/plain; charset=\"utf-8\"\n From: $to");
 ?>
